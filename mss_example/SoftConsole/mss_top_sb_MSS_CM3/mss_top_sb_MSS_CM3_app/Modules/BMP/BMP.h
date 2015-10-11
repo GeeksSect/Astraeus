36762ff67a58385bb4f33a6fedfbba5135fdff5b
Vladimir Artemenko <artemenko.vladimi@gmail.com>
2015-10-11 22:26:25 +0300

    [+] added bmp.c and Helpers

--- mss_example/SoftConsole/mss_top_sb_MSS_CM3/mss_top_sb_MSS_CM3_app/Modules/BMP/BMP.h
+++ mss_example/SoftConsole/mss_top_sb_MSS_CM3/mss_top_sb_MSS_CM3_app/Modules/BMP/BMP.h
@@ -1,46 +1,39 @@
 /*
- * BMP.h
- *
- *  Created on: Oct 11, 2015
- *      Author: Vladimir Artemenko
- */
+ * bmp.h
+ * Module (DOF10)
+ * https://www.adafruit.com/datasheets/BST-BMP180-DS000-09.pdf
+*/
 
 #ifndef BMP_H_
 #define BMP_H_
 
-#define BUFFER_SIZE    32u
+#include "cpu_types.h"
+#include "core_i2c.h"
 
-static uint8_t g_rx_buf[BUFFER_SIZE];
-static uint8_t g_tx_buf[BUFFER_SIZE];
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-static uint8_t g_tx_length = 0x00;
-static uint8_t g_rx_length = 0x00;
 
 /*-------------------------------------------------------------------------
-	* Measure temperature (DOF10)
+ * Initialization of module (DOF10)
 */
-void bmp_get_temperature(uint8_t SLAVE_SER_ADDR, uint8_t* res_buf, uint8_t res_len)
-{
-	i2c_status_t instance;
-	g_tx_buf[0] = 0xF4;
-	g_tx_buf[1] = 0x2E;
-	g_tx_length = 2;
+void BMP_init
+(
+	uint8_t addr
+);
 
-	instance = do_write_transaction(SLAVE_SER_ADDR, g_tx_buf, g_tx_length);
-	//handle_i2c_status(instance, g_tx_buf, g_tx_length);
+/*-------------------------------------------------------------------------
+ * Measure temperature (DOF10)
+*/
+i2c_status_t BMP_get_temperature
+(
+	uint8_t* res_buf,
+	uint8_t res_len
+);
 
-	// Wait 5ms for temperature measurement. Sys_Freq = 50 MHz
-	for (i = 0; i < 50 * 5; i++);
-
-	g_master_tx_buf[0] = 0xF6;
-	g_tx_length = 1;
-
-	instance = do_write_transaction(SLAVE_SER_ADDR, g_tx_buf, g_tx_length);
-	//handle_i2c_status(instance, g_tx_buf, g_tx_length);
-
-	//g_rx_length = 2;
-	instance = do_read_transaction(SLAVE_SER_ADDR, g_rx_buf, res_len);
-	//handle_i2c_status(instance, g_rx_buf, res_len);
+#ifdef __cplusplus
 }
+#endif
 
 #endif /* BMP_H_ */
